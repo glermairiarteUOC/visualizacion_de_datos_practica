cf0eede51f846e71223eb81a1326a99ddd64b06a
Gorka Lerma <glermairiarte@email.com>
2026-01-04 18:28:25 +0100

    Se cambia el nombre de la pagina

--- src/generar_web.py
+++ src/generar_web.py
@@ -118,7 +118,7 @@ def main():
     <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <title>Bitcoin Master Insights</title>
+        <title>Bitcoin Security Analysis</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <style>
             body {{ background-color: #f4f7f6; font-family: 'Segoe UI', sans-serif; }}
